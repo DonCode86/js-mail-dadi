cac018d754e1327c4b91d7a005581f120edbef75
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-03 16:50:09 +0200

    inizio esercizio mail

---
+++ mail/js/main.js
@@ -0,0 +1,8 @@
+const enabledMails = ["d.ciccarelli86@gmail.com","f.brandozzi@gmail.com","m.rossi@gmail.com"];
+const userMail = prompt("Per accedere inserisci la tua mail");
+
+for (let i = 0; i < enabledMails.length; i++) {
+    console.log(enabledMails[i]);
+}
+
+console.log(enabledMails.length);
