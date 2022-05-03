505b24d60d0c3d699dc3e9acd7f991585a53f2aa
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-03 16:55:25 +0200

    aggiunto else mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,9 +2,11 @@ const enabledMails = ["d.ciccarelli86@gmail.com","f.brandozzi@gmail.com","m.ross
 const userMail = prompt("Per accedere inserisci la tua mail");
 
 for (let i = 0; i < enabledMails.length; i++) {
-    console.log(enabledMails[i]);
     if (userMail === enabledMails[i]) {
         console.log("benvenuto")
+    }
+    else {
+        console.log("spiacente, l'email inserita non e' abilitata all'acceso.")
     }
 }
 
