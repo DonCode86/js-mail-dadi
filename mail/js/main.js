518e0db5a8c5a86a95a32c2315cf9fdc8c78c02d
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-03 17:30:47 +0200

    esercizio completato

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,13 +1,16 @@
 const enabledMails = ["d.ciccarelli86@gmail.com","f.brandozzi@gmail.com","m.rossi@gmail.com"];
 const userMail = prompt("Per accedere inserisci la tua mail");
+let enabler = 0;
 
 for (let i = 0; i < enabledMails.length; i++) {
     if (userMail === enabledMails[i]) {
-        console.log("benvenuto")
-    }
-    else {
-        console.log("spiacente, l'email inserita non e' abilitata all'acceso.")
+        enabler = 1;
     }
 }
+if (enabler) {
+    console.log("Benvenuto!")
+}
+else {
+console.log("spiacente, l'email inserita non e' abilitata all'acceso.");
+}
 
-// console.log(enabledMails.length);
