2126697132c50012b49d9b03756010334f945441
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-03 15:20:01 +0200

    first commit

---
+++ dadi/js/main.js
@@ -0,0 +1,23 @@
+const button=document.getElementById("btn");
+
+button.addEventListener("click",
+    function() {
+        const userNumber=Math.floor(Math.random() * 6) + 1;
+        console.log("Hai ottenuto il numero: " + userNumber)
+        let computerNumber = Math.floor(Math.random() * 6) + 1;
+        console.log("Il tuo avversario ha ottenuto il numero: " + computerNumber);
+
+        if (userNumber > computerNumber) {
+            console.log ("Complimenti! Hai vinto!")
+        }
+
+        else if (userNumber < computerNumber) {
+            console.log ("Mi dispiace! Hai perso!")
+        }
+
+        else {
+            console.log ("Wow! Hai pareggiato!")
+        }
+    }
+
+)
